71b0a0122efdf69166877f56b4cb951e32595822
Olatunde Olukoya <115417709+olatunde222@users.noreply.github.com>
2023-05-20 06:01:48 +0400

    Add files via upload

---
+++ Admin Dashboard Website/orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName : "Foldable Mini Drone",
+        productNumber : "85631",
+        paymentStatus : "Due",
+        shipping: "Pending"
+    },
+    {
+        productName : "LARVENDER KF1102 Drone",
+        productNumber : "363767",
+        paymentStatus : "Refunded",
+        shipping: "Declined"
+    },
+    {
+        productName : "Ruko F11 pro Drone",
+        productNumber : "49374",
+        paymentStatus : "Due",
+        shipping: "Pending"
+    },
+    {
+        productName : "Drone with Camera Drone",
+        productNumber : "49347",
+        paymentStatus : "Due",
+        shipping: "Pending"
+    },
+    {
+        productName : "GPS 4K Drone",
+        productNumber : "22821",
+        paymentStatus : "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName : "DJI Air 2S",
+        productNumber : "81475",
+        paymentStatus : "Due",
+        shipping: "Pending"
+    },
+    {
+        productName : "Lozenge Drone",
+        productNumber : "00482",
+        paymentStatus : "Piad",
+        shipping: "Delivered"
+    },
+
+]
